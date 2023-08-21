cad8de098a2215eb60d6bb5c55d413da15f6fd65
Artur Logan <arturlogan1ferreira@gmail.com>
2023-08-21 16:22:30 -0300

    Projeto base criado e testado com sucesso!

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Olá, seja bem vindo ao DIO Bank!");
+
+        System.out.println("Insira seu nome");
+        String nomeCliente = sc.nextLine();
+        
+        System.out.println("Por favor, digite o número da Agência !");
+        int numeroAgencia = sc.nextInt();
+
+        System.out.println("Por favor, digite a Agência !");
+        String nomeAgencia = sc.next();
+
+        System.out.println("Por favor, insira seu Saldo !");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta na DIO Bank, sua agência é " + nomeAgencia + ", conta " + numeroAgencia + ", e seu saldo " + saldo + " já está disponível para saque!");
+        
+        sc.close();
     }
 }
